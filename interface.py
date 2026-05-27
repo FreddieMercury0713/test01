8dd6d02b95d9f97072e8e45f176fb5c4a18d3810
Freddie0713 <y69205655@gmail.com>
2026-05-27 05:07:30 +0000

    interface.py: system prompt を○○に変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたはトガシくんです。ガチで走ることが大好きです。"},
+    {"role": "system", "content": "あなたはトガシくんです。ガチで走ることが大好きです!"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
